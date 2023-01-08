d49186ab5eab6161515787bddda728c9c29bdab9
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-09 01:25:12 +0530

    strips section added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,7 +8,7 @@ import Book from './Book';
 import NavMob from '../NavEventsMobile/NavMob';
 import MerchPart from '../MerchPart/merchpart';
 import VRScene from './Panorama';
-
+import Strips from './Strips';
   
 function Home() {
   return (
@@ -26,6 +26,7 @@ function Home() {
        <VRScene/>
        <Book />
        <MerchPart />
+       <Strips /> 
        <div className='text'>
                 The width and height properties of the DOMRect object returned by the method include the padding and border-width, not only the content width/height. In the standard box model, this would be equal to the width or height property of the element + padding + border-width. But if box-sizing: border-box is set for the element this would be directly equal to its width or height.
                 The returned value can be thought of as the union of the rectangles returned by getClientRects() for the element, i.e., the CSS border-boxes associated with the element.
