8cdd6e57fb433b15de7f92eacc858bef4926b4cb
Geetmanik <geetmanik2@gmail.com>
2023-01-12 23:09:21 +0530

    Panorama final

--- src/Components/Home/Panorama.jsx
+++ src/Components/Home/Panorama.jsx
@@ -1,13 +1,19 @@
-import "aframe";
-import React, { Component } from "react";
-import "./Panorama.css";
+import React, { Component, useState } from 'react';
+import './Panorama.css'
+ 
+import {Pannellum} from "pannellum-react";
+import myImage from "./images/image.jpg";
+
 
 const VRScene = () => {
-    return (
-        <div>
-        <div className="geet">
-            <section className="aframe_container">
-                <a-scene>
+
+
+  
+        return (
+
+             
+        
+              <div >
                     <div className="description">
                         Alcheringa is the annual cultural festival of the Indian
                         Institute of Technology, Guwahati. A splendid idea
@@ -15,18 +21,29 @@ const VRScene = () => {
                         the dawn of one of Asia’s most exhilarating and
                         significant cultural college fests as we know it today.{" "}
                     </div>
-                    <a-sky
-                        src={require("./images/image.png")}
-                        rotation="0 -130 0"
-                    ></a-sky>
-                </a-scene>
-            </section>
-        </div>
-        <div className="blank_div3">
-
-        </div>
-        </div>
-    );
-};
-
-export default VRScene;
+                <Pannellum
+                    width="100%"
+                    height="100vh"
+                    image={myImage}
+                    pitch={10}
+                    yaw={180}
+                    hfov={110}
+                    autoLoad    
+                
+                    autoRotate={8}
+                    showZoomCtrl={false}
+                    showFullscreenCtrl={false}
+                    mouseZoom={false}
+                  
+                />
+                 
+             
+               
+              
+           
+             
+                </div>
+            );
+        }
+        
+        export default VRScene;
