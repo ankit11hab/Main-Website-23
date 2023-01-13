568ef30f83b18800fc66c44d389242e634ddcd7c
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-14 02:56:12 +0530

    book cover carousel

--- src/Components/Home/Carousel.jsx
+++ src/Components/Home/Carousel.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Slider from "react-touch-drag-slider";
+import React, { useEffect, useRef, useState } from "react";
 import vid1 from './bookCoverVideos/book float.webm';
 import vid2 from './bookCoverVideos/cover-pronites animation.webm';
 import vid3 from './bookCoverVideos/creators camp-humor fest animation.webm';
@@ -7,44 +6,88 @@ import vid4 from './bookCoverVideos/humor fest-proshows animation.webm';
 import vid5 from './bookCoverVideos/pronites-creators camp.webm';
 import './BookCover.css';
 
-// Whatever you render out in the Slider will be draggable 'slides'
 function App() {
-    const videos = [
-        {
-            url:vid1,
-            title:'vid1'
-        },
-        {
-            url:vid2,
-            title:'vid1'
-        },
-        {
-            url:vid5,
-            title:'vid1'
-        },
-        {
-            url:vid3,
-            title:'vid1'
-        },
-        {
-            url:vid4,
-            title:'vid1'
-        },
-    ];
+    const videos=[vid1, vid2,vid5, vid3, vid4];
+    var index=0;
+    const videoSrc = useRef();
+    const openBook = useRef();
+    const nextBtn = useRef();
+    const prevBtn = useRef();
+    const timeout=()=>{setTimeout(()=>{
+      prevBtn.current.disabled=false;
+      nextBtn.current.disabled=false;
+      openBook.current.disabled=false;
+    },1700)}
+    function showNext(e){
+      if(index<=5){
+        index=index+1;
+      }
+      if(index>1){
+        prevBtn.current.style.display='block';
+      }
+      if(index<5){
+        videoSrc.current.src=videos[index];
+        prevBtn.current.disabled=true;
+        nextBtn.current.disabled=true;
+        timeout();
+        console.log(videoSrc.current);
+      }
+      if(index===4){
+        nextBtn.current.style.display='none';
+      }
+      return true;
+    }
+    function showNext1(e){
+      index=index+1;
+      openBook.current.style.display='none';
+      prevBtn.current.style.display='none';
+      nextBtn.current.style.display='block';
+        videoSrc.current.src=videos[index];
+        prevBtn.current.setAttribute('disabled','true');
+        nextBtn.current.disabled=true;
+        timeout();
+      }
+      function showPrevious(e){
+        if(index>=0){
+          index=index-1;
+        }
+        if(index<=1){
+          prevBtn.current.style.display='none';
+        }
+        if(0<=index<=4){
+        nextBtn.current.style.display='block';
+        videoSrc.current.src=videos[index];
+        prevBtn.current.disabled=true;
+        nextBtn.current.disabled=true;
+        timeout();
+      }
+    }
+    useEffect(()=>{
+      if(index===0){
+        openBook.current.style.display='block';
+        openBook.current.disabled=true;
+        prevBtn.current.style.display='none';
+        nextBtn.current.style.display='none';
+        const f= ()=>{
+          timeout(()=>{
+            openBook.current.disabled=false;
+          })
+        }
+        f();
+      }
+    });
   return (
-    <main>
-      <Slider autoPlay loop
-      activeIndex={0}
-      threshHold={2}
-      transition={0.1}
-      >
-        {videos.map(({ url, title }, index) => (
-          <video autoPlay muted>
-            <source src={url}></source>
-          </video>
-        ))}
-      </Slider>
-    </main>
+    <div className="main_cont">
+          <div className="video_container">
+            <video id="vid1" ref={videoSrc} src={videos[index]} autoPlay muted>
+            </video>
+          </div>
+          <div className="buttons">
+            <button ref={openBook} className='openbookBtn' onClick={(e)=>{showNext1()}}>Click to Open</button>
+            <button className='prevBtn' onClick={(e)=>{showPrevious()}} ref={prevBtn}>PREVIOUS</button>
+            <button className='nxtBtn' onClick={(e)=>{showNext()}} ref={nextBtn}>NEXT</button>
+          </div>
+    </div>
   );
 }
 
